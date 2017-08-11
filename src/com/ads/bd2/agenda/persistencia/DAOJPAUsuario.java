446e1a5474e11126265eee933eb39747d22acab4
Jadson <Jadson@Jadson-PC>
2017-08-11 11:13:22 -0300

    Criação DAO Usuario

---
+++ src/com/ads/bd2/agenda/persistencia/DAOJPAUsuario.java
@@ -0,0 +1,19 @@
+package com.ads.bd2.agenda.persistencia;
+
+import javax.persistence.EntityManager;
+import com.ads.bd2.agenda.modelo.Usuario;
+
+public class DAOJPAUsuario extends DAOJPA<Usuario> {
+
+	public DAOJPAUsuario(EntityManager em) {
+		super(em);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Class<Usuario> getDAOClass() {
+		// TODO Auto-generated method stub
+		return Usuario.class;
+	}
+
+}
