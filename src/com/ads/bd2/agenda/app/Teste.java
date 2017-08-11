1da30932e1359d855b0056fc0ec8f1cae18b2c8e
Jadson <Jadson@Jadson-PC>
2017-08-11 13:23:22 -0300

    Teste com Usuario

--- src/com/ads/bd2/agenda/app/Teste.java
+++ src/com/ads/bd2/agenda/app/Teste.java
@@ -1,12 +1,36 @@
 package com.ads.bd2.agenda.app;
 
+import java.sql.Date;
+
+import javax.persistence.EntityManager;
+
 import com.ads.bd2.agenda.modelo.Usuario;
+import com.ads.bd2.agenda.persistencia.DAOJPA;
+import com.ads.bd2.agenda.persistencia.DAOJPAUsuario;
 
 public class Teste {
 
 	public static void main(String[] args) {
+		//CRIANDO ENTITY MANAGER, INICIANDO TRANSAÇÃO E INSTANCIANDO DAO DO USUARIO
+		EntityManager em = DAOJPA.createAndInitEntityManager();
+		em.getTransaction().begin();
+		DAOJPAUsuario daoUsuario= new DAOJPAUsuario(em);
+		
+		//CRIANDO USUARIOS E INSERINDO DENTRO DA TRANSAÇÃO (PERSISTENCIA)
+		for(int i=0; i<1000; i++) {
 		Usuario usuario = new Usuario();
-
+		usuario.setLogin("jadson"+i+1);
+		usuario.setNome("Jadson");
+		usuario.setSenha("123");
+		usuario.setEmail("jadson"+i+"@gmail.com");
+		usuario.setDataNascimento(new Date(System.currentTimeMillis()));
+		
+		daoUsuario.create(usuario);
+		}
+		
+		//EFETUANDO TRANSAÇÃO E FECHANDO ENTITY MANAGER
+		em.getTransaction().commit();
+		daoUsuario.closeEntityManager();
 	}
 
 }
