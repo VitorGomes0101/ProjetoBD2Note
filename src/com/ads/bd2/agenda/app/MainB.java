b5d0fcb1f4d0b54fb5094eba8eca5398317cc1fc
Jadson <Jadson@Jadson-PC>
2017-08-11 18:00:20 -0300

    Criação de Main para cada tópico

---
+++ src/com/ads/bd2/agenda/app/MainB.java
@@ -0,0 +1,76 @@
+package com.ads.bd2.agenda.app;
+
+import javax.persistence.EntityManager;
+
+import com.ads.bd2.agenda.persistencia.DAOJPA;
+import com.ads.bd2.agenda.persistencia.DAOJPALembrete;
+
+public class MainB {
+	public static void main(String[] args) {
+		System.out.println(create());
+		System.out.println(retrieve());
+		System.out.println(update());
+		System.out.println(delete());
+	}
+	
+	private static String create() {	
+		long tempoInicial = System.nanoTime();
+		//CRIANDO ENTITY MANAGER, INICIANDO TRANSAÇÃO E INSTANCIANDO DAO DO USUARIO
+		EntityManager em = DAOJPA.createAndInitEntityManager();
+		em.getTransaction().begin();
+		DAOJPALembrete daoLembrete= new DAOJPALembrete(em);
+		
+		
+		
+		//EFETUANDO TRANSAÇÃO E FECHANDO ENTITY MANAGER
+		em.getTransaction().commit();
+		daoLembrete.closeEntityManager();		
+		return ("O tempo da operação CREATE foi: "+ (System.nanoTime() - tempoInicial)+" nanosegundos");
+	}
+	
+	private static String retrieve() {
+		long tempoInicial = System.nanoTime();
+		//CRIANDO ENTITY MANAGER, INICIANDO TRANSAÇÃO E INSTANCIANDO DAO DO USUARIO
+		EntityManager em = DAOJPA.createAndInitEntityManager();
+		em.getTransaction().begin();
+		DAOJPALembrete daoLembrete= new DAOJPALembrete(em);
+		
+		
+		
+		//EFETUANDO TRANSAÇÃO E FECHANDO ENTITY MANAGER
+		em.getTransaction().commit();
+		daoLembrete.closeEntityManager();		
+		return ("O tempo da operação RETRIEVE foi: "+ (System.nanoTime() - tempoInicial)+" nanosegundos");
+	}
+	
+	private static String update() {
+		long tempoInicial = System.nanoTime();
+		//CRIANDO ENTITY MANAGER, INICIANDO TRANSAÇÃO E INSTANCIANDO DAO DO USUARIO
+		EntityManager em = DAOJPA.createAndInitEntityManager();
+		em.getTransaction().begin();
+		DAOJPALembrete daoLembrete= new DAOJPALembrete(em);
+		
+		
+		
+		//EFETUANDO TRANSAÇÃO E FECHANDO ENTITY MANAGER
+		em.getTransaction().commit();
+		daoLembrete.closeEntityManager();		
+		return ("O tempo da operação UPDATE foi: "+ (System.nanoTime() - tempoInicial)+" nanosegundos");
+	}
+	
+	private static String delete() {
+		long tempoInicial = System.nanoTime();
+		//CRIANDO ENTITY MANAGER, INICIANDO TRANSAÇÃO E INSTANCIANDO DAO DO USUARIO
+		EntityManager em = DAOJPA.createAndInitEntityManager();
+		em.getTransaction().begin();
+		DAOJPALembrete daoLembrete= new DAOJPALembrete(em);
+		
+		
+		
+		//EFETUANDO TRANSAÇÃO E FECHANDO ENTITY MANAGER
+		em.getTransaction().commit();
+		daoLembrete.closeEntityManager();		
+		return ("O tempo da operação DELETE foi: "+ (System.nanoTime() - tempoInicial)+" nanosegundos");
+	}
+
+}
