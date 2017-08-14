36d9518589e58fcd37f684d6bb25df7c4611ee9e
Jadson <Jadson@Jadson-PC>
2017-08-14 18:54:28 -0300

    Método de busca de lembretes em comum

--- src/com/ads/bd2/agenda/persistencia/DAOJPALembrete.java
+++ src/com/ads/bd2/agenda/persistencia/DAOJPALembrete.java
@@ -1,7 +1,14 @@
 package com.ads.bd2.agenda.persistencia;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import com.ads.bd2.agenda.modelo.Lembrete;
+import com.ads.bd2.agenda.modelo.Usuario;
 
 public class DAOJPALembrete extends DAOJPA<Lembrete> {
 
@@ -43,4 +50,34 @@ public class DAOJPALembrete extends DAOJPA<Lembrete> {
 		}
 	}
 
+	//retorna todos os lembretes compartilhados entre todos os usuariosCompartilhando
+	public List<Lembrete> retrieveLembretes(List<Usuario> usuariosCompartilhando){
+		List<Lembrete> lembretes = new ArrayList<Lembrete>();
+		
+		String q = "";
+		if(usuariosCompartilhando.size()>0) {
+			q ="SELECT id_lembrete FROM lembrete_usuario WHERE login_usuario LIKE '"+usuariosCompartilhando.get(0).getLogin()+"' ";
+		}
+		if(usuariosCompartilhando.size()>1) {
+			for(int i=1;i<usuariosCompartilhando.size();i++) {
+				q += "INTERSECT SELECT id_lembrete FROM lembrete_usuario WHERE login_usuario LIKE '"+usuariosCompartilhando.get(i).getLogin()+"' ";
+			}
+		}
+		q+=";";
+		
+		Query query = em.createNativeQuery(q);
+		List<Long> idlembretes = query.getResultList();
+		for(Long i: idlembretes) {
+			Lembrete lembrete = retrieve(i);
+			lembretes.add(lembrete);
+		}
+		
+			return lembretes;
+	}
+	
+	//retorna todos os lembretes de idUsuario que possuem notificações a sarem feitas num dado intervalo de tempo
+	public List<Lembrete> retrieveLembretes(Long idUsuario, Date dataHoraLembrarInicial, Date dataHoraLembraFinal){
+		return null;
+	}
+	
 }
